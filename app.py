de013d4e98b7eeb3265aff549ce650075e4dff2c
Peter Huynh <peterhyh@yahoo.com>
2023-08-22 20:09:16 -0500

    comments removal

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ dest_dir_documents = '/Users/PeterHuynh/Desktop/Downloads/Documents'
 
 image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
                     ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
-# ? supported Video types
+
 video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
                     ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
-# ? supported Audio types
+
 audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
-# ? supported Document types
+
 document_extensions = [".doc", ".docx", ".odt",
                        ".pdf", ".xls", ".xlsx", ".ppt", ".pptx", ".zip", ".stl"]
 
@@ -48,8 +48,6 @@ def move_file(dest, entry, name):
 
 
 class MoverHandler(FileSystemEventHandler):
-    # ? THIS FUNCTION WILL RUN WHENEVER THERE IS A CHANGE IN "source_file"
-    # ? .upper is for not missing out on files with uppercase extensions
     def on_modified(self, event):
         with scandir(source_file) as entries:
             for entry in entries:
@@ -59,29 +57,29 @@ class MoverHandler(FileSystemEventHandler):
                 self.check_image_files(entry, name)
                 self.check_document_files(entry, name)
 
-    def check_audio_files(self, entry, name):  # * Checks all Audio Files
+    def check_audio_files(self, entry, name):
         for audio_extension in audio_extensions:
             if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
-                if entry.stat().st_size < 10_000_000 or "SFX" in name:  # ? 10Megabytes
+                if entry.stat().st_size < 10_000_000 or "SFX" in name:
                     dest = dest_dir_sfx
                 else:
                     dest = dest_dir_music
                 move_file(dest, entry, name)
                 logging.info(f"Moved audio file: {name}")
 
-    def check_video_files(self, entry, name):  # * Checks all Video Files
+    def check_video_files(self, entry, name):
         for video_extension in video_extensions:
             if name.endswith(video_extension) or name.endswith(video_extension.upper()):
                 move_file(dest_dir_video, entry, name)
                 logging.info(f"Moved video file: {name}")
 
-    def check_image_files(self, entry, name):  # * Checks all Image Files
+    def check_image_files(self, entry, name):
         for image_extension in image_extensions:
             if name.endswith(image_extension) or name.endswith(image_extension.upper()):
                 move_file(dest_dir_image, entry, name)
                 logging.info(f"Moved image file: {name}")
 
-    def check_document_files(self, entry, name):  # * Checks all Document Files
+    def check_document_files(self, entry, name):
         for documents_extension in document_extensions:
             if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
                 move_file(dest_dir_documents, entry, name)
